050c1945e984cec055241d313d82a177bf263b52
Zach Costa <zach.costa@willowtreeapps.com>
2015-10-06 11:14:21 -0400

    replace with obj-c

---
+++ travis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  travis
+//
+//  Created by Zach Costa on 10/6/15.
+//  Copyright © 2015 WillowTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ travis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  travis
+//
+//  Created by Zach Costa on 10/6/15.
+//  Copyright © 2015 WillowTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
